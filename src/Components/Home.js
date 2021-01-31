f57fef1aa86f338c56c87bbf8d025cbefc3b6a64
MohamedMamlokAli <mohamedmamlokali@gmail.com>
2021-01-31 16:31:11 +0200

    homepage fully done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,18 +2,23 @@ import React,{useEffect} from 'react'
 import computer from "./computer.svg"
 import keyboard from "./keyboard.svg"
 import mouse from "./mouse.svg"
+import minster from "./minister.svg"
+import govern from "./govern.svg"
 import {NavLink} from "react-router-dom"
 import gsap from "gsap"
-import { Timeline,SplitText } from 'gsap/gsap-core'
-import { random } from 'gsap/gsap-core'
+import { Timeline } from 'gsap/gsap-core'
 const Home = () => {
  useEffect(() => {
   const tl = new Timeline()
+  const logotl= new Timeline({repeat:-1})
   tl.from(".head",{duration:1,opacity :0,x:-100,ease: "sine.out",stagger:.2})
   .from(".btn",{duration:1,y:100,opacity:0,stagger:.25,overflow:"hidden"},"-=.5")
   tl.from(".pc-img",{duration:1,opacity:0,y:-100},"-=2.5")
   .from(".keyboard",{duration:1,opacity:0,x:-100},"-=2")
   .from(".mouse",{duration:1,opacity:0,x:100},"-=1.5")
+  logotl.from(".swap",{duration:2,opacity:0,display:"block"},"")
+  .to(".swap",{duration:2,opacity:0,display:"block"},"+=2").from(".swap1",{duration:2,opacity:0,display:"block"},)
+  .to(".swap1",{duration:2,opacity:0,display:"block"},"+=2")
  }, [])
  return (
   <section className="section">
@@ -36,6 +41,12 @@ const Home = () => {
     </div>
     <div className="left-content">
      <div className="pc-img">
+      <div className="swap">
+      <img className="swap" src={govern} alt=""/>
+      </div>
+      <div className="swap1">
+        <img className="swap1" src={minster} alt=""/>
+      </div>
       <img src={computer} alt=""/>
       </div>
      <div className="mouse_keyboard">
